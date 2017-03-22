aaf3b6e938cee1f69a9f066201516f0b3aae9205
joellegg <joellegg@bellsouth.net>
2017-03-22 15:28:22 -0500

    averaging stock prices for user company over past year complete

--- stocks.js
+++ stocks.js
@@ -4,8 +4,6 @@ const { get } = require('http');
 
 let [,,input] = process.argv;
 
-console.log('input', input);
-
 // http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22AAPL%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D
 get(`http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/JSON?parameters={"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"${input}","Type":"price","Params":["c"]}]}`, (res) => {
   const statusCode = res.statusCode;
@@ -33,8 +31,15 @@ get(`http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/JSON?paramete
   res.on('data', (chunk) => rawData += chunk);
   res.on('end', () => {
     try {
+      let sum = 0;
       let parsedData = JSON.parse(rawData);
-      console.log(parsedData);
+      for (let i = 0; i < parsedData.Elements[0].DataSeries.close.values.length; i++) {
+        sum += parsedData.Elements[0].DataSeries.close.values[i];
+      }
+      let avgPrice = (sum/parsedData.Elements[0].DataSeries.close.values.length);
+      console.log(`The average price for ${input} over the past year was`, avgPrice.toFixed(2))
+
+      // console.log(parsedData.Elements[0].DataSeries.close.values)
     } catch (e) {
       console.log(e.message);
     }
