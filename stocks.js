94def6d7d043cf28395a72ba38a03ddc9676edce
joellegg <joellegg@bellsouth.net>
2017-03-22 14:16:24 -0500

    converted unicode characters

--- stocks.js
+++ stocks.js
@@ -2,7 +2,8 @@
 
 const { get } = require('http');
 
-get('http://nodejs.org/dist/index.json', (res) => {
+// http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22AAPL%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D
+get('http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"AAPL","Type":"price","Params":["c"]}]}', (res) => {
   const statusCode = res.statusCode;
   const contentType = res.headers['content-type'];
 
@@ -16,7 +17,9 @@ get('http://nodejs.org/dist/index.json', (res) => {
   }
   if (error) {
     console.log(error.message);
-    // consume response data to free up memory, since we won't use the request body. Until the data is consumed, the 'end' event will not fire. Also, until the data is read it will consume memory that can eventually lead to a 'process out of memory' error.
+    // consume response data to free up memory, since we won't use the request body.
+    // Until the data is consumed, the 'end' event will not fire.
+    // Also, until the data is read it will consume memory that can eventually lead to a 'process out of memory' error.
     res.resume();
     return;
   }
