1d44e03cb5f5828d1442c09c2b7ea66a2a8f934d
joellegg <joellegg@bellsouth.net>
2017-03-22 13:40:09 -0500

    set up project with readme and copied in the request and response streams from node

---
+++ stocks.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const { get } = require('http');
+
+get('http://nodejs.org/dist/index.json', (res) => {
+  const statusCode = res.statusCode;
+  const contentType = res.headers['content-type'];
+
+  let error;
+  if (statusCode !== 200) {
+    error = new Error(`Request Failed.\n` +
+                      `Status Code: ${statusCode}`);
+  } else if (!/^application\/json/.test(contentType)) {
+    error = new Error(`Invalid content-type.\n` +
+                      `Expected application/json but received ${contentType}`);
+  }
+  if (error) {
+    console.log(error.message);
+    // consume response data to free up memory, since we won't use the request body. Until the data is consumed, the 'end' event will not fire. Also, until the data is read it will consume memory that can eventually lead to a 'process out of memory' error.
+    res.resume();
+    return;
+  }
+
+  res.setEncoding('utf8');
+  let rawData = '';
+  res.on('data', (chunk) => rawData += chunk);
+  res.on('end', () => {
+    try {
+      let parsedData = JSON.parse(rawData);
+      console.log(parsedData);
+    } catch (e) {
+      console.log(e.message);
+    }
+  });
+}).on('error', (e) => {
+  console.log(`Got error: ${e.message}`);
+});
