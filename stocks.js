4871119f1e3a71e3fee90b98657721b0c026f980
joellegg <joellegg@bellsouth.net>
2017-03-22 14:37:23 -0500

    getting stock selection from user

--- stocks.js
+++ stocks.js
@@ -2,8 +2,12 @@
 
 const { get } = require('http');
 
+let [,,input] = process.argv;
+
+console.log('input', input);
+
 // http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22AAPL%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D
-get('http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"AAPL","Type":"price","Params":["c"]}]}', (res) => {
+get(`http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/JSON?parameters={"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"${input}","Type":"price","Params":["c"]}]}`, (res) => {
   const statusCode = res.statusCode;
   const contentType = res.headers['content-type'];
 
@@ -11,7 +15,7 @@ get('http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?paramete
   if (statusCode !== 200) {
     error = new Error(`Request Failed.\n` +
                       `Status Code: ${statusCode}`);
-  } else if (!/^application\/json/.test(contentType)) {
+  } else if (!/^text\/javascript/.test(contentType)) {
     error = new Error(`Invalid content-type.\n` +
                       `Expected application/json but received ${contentType}`);
   }
